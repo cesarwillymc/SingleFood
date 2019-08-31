9e78ba7f016af7774fa519803044f8a2a835391f
cesarwillymc <48421004+cesarwillymc@users.noreply.github.com>
2019-08-30 23:19:57 -0500

    ultimo commit

--- app/src/main/java/com/singlefood/sinfo/models/informacion_platillos.java
+++ app/src/main/java/com/singlefood/sinfo/models/informacion_platillos.java
@@ -1,12 +1,16 @@
 package com.singlefood.sinfo.models;
 
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -30,23 +34,21 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.singlefood.sinfo.R;
 import com.singlefood.sinfo.models.productos.Comentarios;
-import com.singlefood.sinfo.models.productos.Platillos;
 import com.singlefood.sinfo.models.productos.RecyclerComentariosAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class informacion_platillos extends AppCompatActivity {
+public class informacion_platillos extends AppCompatActivity implements View.OnClickListener{
 
     private ArrayList<String> datos;
     String Key;
     private RecyclerView.Adapter adapterRview;
-
-    private ArrayList<Comentarios> comentariosPlatillos;
-    ArrayList<Platillos> arrayListPlatillos;
+    private ProgressDialog progressDialog;
     @BindView( R.id.appbar_info )
     AppBarLayout appBarLayout;
     @BindView( R.id.collapsing_toolbar_info )
@@ -67,6 +69,12 @@ public class informacion_platillos extends AppCompatActivity {
     RecyclerView Rview_info;
     @BindView( R.id.rating_bar_info_platillos )
     RatingBar ratingBar;
+    @BindView( R.id.button_informacion_platillos )
+    Button button_coment;
+    @BindView( R.id.edit_text_informacion_platillos )
+    EditText editText_coment;
+    @BindView( R.id.rating_bar_informacion_platillos )
+    RatingBar rating_coment;
 
     RecyclerView recyclerView_info;
 
@@ -79,6 +87,7 @@ public class informacion_platillos extends AppCompatActivity {
             getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN );
         }
+        button_coment.setOnClickListener( this );
         obtener_datos();
         getCommts();
         configToolbar();
@@ -104,7 +113,8 @@ public class informacion_platillos extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if (task.isSuccessful()){
-                    Toast.makeText(informacion_platillos.this,"Entro: "+task.getResult() ,Toast.LENGTH_SHORT ).show();
+                   // Toast.makeText(informacion_platillos.this,"Entro: "+task.getResult() ,Toast.LENGTH_SHORT ).show();
+                    progressDialog.dismiss();
                 }
 
             }
@@ -126,19 +136,19 @@ public class informacion_platillos extends AppCompatActivity {
 
     private void getCommts() {
         DatabaseReference coment= FirebaseDatabase.getInstance().getReference("Platillos").child( Key ).child( "Comentarios" );
-        comentariosPlatillos= new ArrayList<>(  );
-        coment.addListenerForSingleValueEvent( new ValueEventListener() {
+
+        coment.addValueEventListener( new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
+                ArrayList<Comentarios> arrayListComentarios= new ArrayList<>();
                 for (DataSnapshot snapshot: dataSnapshot.getChildren()){
                     Comentarios coment=snapshot.getValue(Comentarios.class);
-                    comentariosPlatillos.add( coment );
+                    arrayListComentarios.add( coment );
                 }
-                ratingBar.setRating( promedioRating());
+                ratingBar.setRating( promedioRating(arrayListComentarios));
                 //Toast.makeText( informacion_platillos.this,"key: "+ comentariosPlatillos.get( 0 ).getTexto(),Toast.LENGTH_SHORT ).show();
                 //Toast.makeText( informacion_platillos.this,"Prueba: "+comentariosPlatillos.size(),Toast.LENGTH_SHORT ).show();
-                adapterRview = new RecyclerComentariosAdapter( informacion_platillos.this, R.layout.rv_comentarios_items, comentariosPlatillos, new RecyclerComentariosAdapter.OnItemClickListener2() {
+                adapterRview = new RecyclerComentariosAdapter( informacion_platillos.this, R.layout.rv_comentarios_items, arrayListComentarios, new RecyclerComentariosAdapter.OnItemClickListener2() {
                     @Override
                     public void OnClickListener2(Comentarios comentarios, int adapterPosition) {
                         Toast.makeText( informacion_platillos.this,"Prueba: "+comentarios.getTexto(),Toast.LENGTH_SHORT ).show();
@@ -163,14 +173,14 @@ public class informacion_platillos extends AppCompatActivity {
 
 
     }
-    public Float promedioRating(){
+    public Float promedioRating(ArrayList<Comentarios> comentariosPlatillos){
         float temp=0;
-        Toast.makeText( this,"Tamanñ array: "+ comentariosPlatillos.size(),Toast.LENGTH_SHORT ).show();
+       // Toast.makeText( this,"Tamanñ array: "+ comentariosPlatillos.size(),Toast.LENGTH_SHORT ).show();
         for (int i=0;i<comentariosPlatillos.size();i++){
             temp=temp+comentariosPlatillos.get(  i).getRating();
-            Toast.makeText( this,"rating: "+ temp,Toast.LENGTH_SHORT ).show();
+            //Toast.makeText( this,"rating: "+ temp,Toast.LENGTH_SHORT ).show();
         }
-        Toast.makeText( this,"rating complete: "+ temp,Toast.LENGTH_SHORT ).show();
+       //Toast.makeText( this,"rating complete: "+ temp,Toast.LENGTH_SHORT ).show();
         return temp/comentariosPlatillos.size();
 
     }
@@ -197,4 +207,21 @@ public class informacion_platillos extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button_informacion_platillos:
+                    progressDialog=new ProgressDialog( this );
+                    progressDialog.setCancelable( false );
+                    progressDialog.setMessage( "Cargando tu comentarioo..." );
+                    progressDialog.show();
+                    Map<String,Object> map = new HashMap<>(  );
+                    map.put( "id_comentarios","Prueba001" );
+                    map.put( "rating",rating_coment.getRating() );
+                    map.put( "texto",editText_coment.getText().toString().trim() );
+                    PublicComment( map );
+
+                break;
+        }
+    }
 }
